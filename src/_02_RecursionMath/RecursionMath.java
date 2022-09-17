60fb7a97ecf6243aa88c46fb9edc4ba6eefe96d2
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2022-09-17 12:57:41 -0700

    finished level 3

--- src/_02_RecursionMath/RecursionMath.java
+++ src/_02_RecursionMath/RecursionMath.java
@@ -2,37 +2,42 @@ package _02_RecursionMath;
 
 public class RecursionMath {
 
-    public static int recursiveMultiplication(int number, int times) {
-        // If times is 1 
-    	if(times == 1) {
-    		return number;
-    	}
-            // Return number 
+	public static int recursiveMultiplication(int number, int times) {
+		// If times is 1
+		if (times == 1) {
+			return number;
+		}
+		// Return number
 
-        // Else return number + recursiveMultiplication(number, times-1)
-    	else {
-    		return number+recursiveMultiplication(number, times-1);
-    	}
-        
-    }
+		// Else return number + recursiveMultiplication(number, times-1)
+		else {
+			return number + recursiveMultiplication(number, times - 1);
+		}
 
-    // Try this one on your own! 
-    // Hint: if numberToDivideBy is bigger than number,
-    //       you can't divide anymore
-    public static int recursiveDivision(int number, int numberToDivideBy) {
-        if(numberToDivideBy==number) {
-        	
-        	return 1;
-        }
-        else {
-        	return number/recursiveDivision(number,numberToDivideBy);
-        }
-        
-    }
+	}
 
-    // Try this one on your own!
-    public static int recursivePower(int number, int power) {
-        
-        return 0;
-    }
+	// Try this one on your own!
+	// Hint: if numberToDivideBy is bigger than number,
+	// you can't divide anymore
+	public static int recursiveDivision(int number, int numberToDivideBy) {
+		if (numberToDivideBy > number) {
+
+			return 0;
+		} else {
+			return 1 + recursiveDivision(number-numberToDivideBy, numberToDivideBy);
+		}
+
+	}
+
+	// Try this one on your own!
+	public static int recursivePower(int number, int power) {
+		if (power <= 1) {
+			return number;
+
+		}
+
+		else {
+			return number *recursivePower(number, power-1);
+		}
+	}
 }
